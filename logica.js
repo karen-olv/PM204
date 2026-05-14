f7444ec6e0ec640475b04d8356dbf0a5651cf733
karen-olv <123047625@upq.edu.mx>
2026-05-13 23:06:33 -0600

    Cocina
    Módulo de Cocina desarrollado en JavaScript para un sistema de cafetería, encargado de administrar y mostrar el catálogo de productos mediante objetos, arreglos y módulos conectados con Node.js.

--- logica.js
+++ logica.js
@@ -1,3 +1,11 @@
-let nombre = prompt("¿cual es tu nombre?")
-        console.log("hola  " + nombre + "!")
-        document.write("hola " + nombre + "!")
+let catalogo = [
+    { nombre: "Café Americano", precio: 40 },
+    { nombre: "Capuchino", precio: 55 },
+    { nombre: "Latte Vainilla", precio: 60 },
+    { nombre: "Muffin de Chocolate", precio: 35 },
+    { nombre: "Té Verde", precio: 45 }
+];
+
+console.log("--- MENU DE COFFEE CODE I ---");
+
+console.table(catalogo);
